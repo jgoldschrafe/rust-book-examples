396bc5c22f3455c2dd3487a0d68124642a4de62d
Jeff Goldschrafe <jeff@rabb.it>
2019-05-23 17:30:10 -0400

    Updates

--- 5.2_an_example_program_using_structs/src/main.rs
+++ 5.2_an_example_program_using_structs/src/main.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!("rect1 is {:#?}", rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
---
+++ 6.1_enums_and_pattern_matching/src/main.rs
@@ -0,0 +1,24 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route(ip_kind: IpAddrKind) { }
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+}

9e1ab913d252dfd863fadeee51cf244793779cf9
Jeff Goldschrafe <jeff@holyhandgrenade.org>
2019-05-24 09:51:36 -0400

    Update through 10.3

---
+++ 10.3_lifetime_syntax/src/main.rs
@@ -0,0 +1,24 @@
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+}
